786b49be5ff572f541c4262f015363845fee24e0
Garrett Howell <garrett.howell74@gmail.com>
2018-05-30 10:21:16 -0400

    just the first board with some moving enemies

---
+++ Assets/Enemy.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public float speed = 10f;
+
+    private Transform target;
+    private int wavePointIndex = 0;
+
+    private void Start()
+    {
+        target = Waypoints.points[0];
+    }
+
+    private void Update()
+    { 
+        if (target == null)
+        {
+            Debug.Log("Hel");
+            target = Waypoints.points[0];
+        }  
+        Vector3 direction = target.position - transform.position;
+        transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+       
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.4f)
+        {
+            GetNextWaypoint();
+        }
+    }
+
+    private void GetNextWaypoint()
+    {
+        if (wavePointIndex >= Waypoints.points.Length - 1)
+        {
+            
+            Destroy(gameObject);
+            return;
+        
+        }
+
+        wavePointIndex++;
+        target = Waypoints.points[wavePointIndex];
+    }
+}
